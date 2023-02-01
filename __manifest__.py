573e9228de3c260d83188f5b863eafcba4b050af
Bnaard <bnaard@gmx.net>
2023-02-01 21:28:41 +0000

    Minimum module

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "MyFirstModule",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Bnaard",
+    'category': 'Category',
+    'application': True,
+    'description': """
+    Description text
+    """,
+}
